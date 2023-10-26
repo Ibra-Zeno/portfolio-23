b17d6af3c28be8148d90043457b609fdb7ced889
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-26 21:30:55 +0100

    Padding

--- components/About.tsx
+++ components/About.tsx
@@ -22,9 +22,9 @@ const features = [
 const About: React.FC = () => {
   return (
     <div className="overflow-hidden bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl px-3 sm:px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="lg:pr-8 lg:pt-4">
+          <div className="px-6 sm:px-0 lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                 A better workflow
