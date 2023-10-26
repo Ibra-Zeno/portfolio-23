68658745bf4ef73b7e80b03fb6914e426e193e4a
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-26 21:31:26 +0100

    Simple mobile nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -24,20 +24,24 @@ const Nav: React.FC = () => {
   const menuItems = ["Profile", "Dashboard"];
 
   return (
-    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} className="custom-font">
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-        />
-      </NavbarContent>
-
-      <NavbarContent className="relative pr-3 sm:hidden" justify="start">
+    <Navbar
+      isBordered
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
+      className="custom-font"
+    >
+      <NavbarContent
+        className="relative mx-auto pr-3 sm:hidden"
+        justify="center"
+      >
         <NavbarBrand>
-          <Image
-            src="/images/ik-logo.jpg"
-            className="max-w-10 absolute top-0 flex max-h-10 justify-center rounded-full"
-            alt="IK logo"
-          ></Image>
+          <Link href="/">
+            <Image
+              src="/images/ik-logo.jpg"
+              className="max-w-10 max-h-10 rounded-full"
+              alt="IK logo"
+            ></Image>
+          </Link>
         </NavbarBrand>
       </NavbarContent>
 
@@ -125,88 +129,8 @@ const Nav: React.FC = () => {
           </DropdownMenu>
         </Dropdown>
       </NavbarContent>
-
-      {/*       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="warning" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent> */}
-
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={
-                index === 2
-                  ? "warning"
-                  : index === menuItems.length - 1
-                  ? "danger"
-                  : "foreground"
-              }
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
     </Navbar>
   );
 };
 
 export default Nav;
-
-/* <header className="body-font text-gray-600">
-      <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
-        <Link
-          href="/"
-          className="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="h-10 w-10 rounded-full bg-indigo-500 p-2 text-white"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <span className="ml-3 text-xl">{logoText}</span>
-        </Link>
-        <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto">
-          {links.map((link, idx) => (
-            <Link
-              className="mr-5 hover:text-gray-900"
-              href={`/${link.toLowerCase().replace(/\s+/g, "-")}`}
-              key={idx}
-            >
-              {link}
-            </Link>
-          ))}
-        </nav>
-        <button className="mt-4 inline-flex items-center rounded border-0 bg-gray-100 px-3 py-1 text-base hover:bg-gray-200 focus:outline-none md:mt-0">
-          Button
-          <svg
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="ml-1 h-4 w-4"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
-      </div>
-    </header> */
