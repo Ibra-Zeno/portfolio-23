58625d01cd45d738aed652dbef92e2a79108c29b
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-26 23:45:56 +0100

    Minor design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,17 +5,18 @@ import { Instagram, Github, Twitter } from "lucide-react";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-white">
-      <div className="mx-auto max-w-7xl overflow-hidden px-4 py-12 sm:px-6 lg:px-8">
+      <div className="mx-auto overflow-hidden border-t border-gray-300 px-4 py-7 sm:px-6 lg:px-8">
         <div className="mx-auto flex items-center justify-center text-center text-2xl font-bold">
           <Link href="/" className="flex h-fit w-fit justify-center">
             <Image
               src="/images/ik-logo.jpg"
               className="h-10 w-10 rounded-full"
+              draggable={false}
               alt="IK logo"
             ></Image>
           </Link>
         </div>
-        <div className="mt-4 flex justify-center space-x-6">
+        <div className="mt-5 flex justify-center space-x-6">
           <span className="m-auto inline-flex w-full justify-center gap-3 md:w-auto md:justify-start">
             <Link
               href="#"
@@ -42,15 +43,14 @@ const Footer: React.FC = () => {
         </div>
         <p className="mt-3 text-center">
           <span className="mx-auto mt-2 text-sm text-gray-500">
-            Copyright © 2020 - 2021
+            Made by yours truly,
             <Link
               href="#"
-              className="mx-2 text-blue-500 hover:text-gray-500"
+              className="mx-1 text-blue-500 hover:text-gray-500"
               rel="noopener noreferrer"
             >
-              @unwrappedHQ
+              Ibrahim Kalam
             </Link>
-            .Since 2020
           </span>
         </p>
       </div>
