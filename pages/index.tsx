eb62653829aaef600ff18d6bb0a4f6f03582a8d5
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-28 18:17:15 +0100

    Contact import

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import TechStack from "@/components/TechStack";
 import Projects from "@/components/Projects";
 import Footer from "@/components/Footer";
-import About from "@/components/About";
+import About from "@/components/Contact";
 
 export default function Home() {
   return (
