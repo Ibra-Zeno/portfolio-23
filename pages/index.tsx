952f56aec0fd72777c0a245ed1164a5ea8e3108c
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-28 23:49:56 +0100

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,18 @@ import Hero from "@/components/Hero";
 import TechStack from "@/components/TechStack";
 import Projects from "@/components/Projects";
 import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
 import About from "@/components/Contact";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <TechStack />
-      <Projects />
-      <About />
-      <Footer />
+      <Layout>
+        <Hero />
+        <TechStack />
+        <Projects />
+        <About />
+      </Layout>
     </>
   );
 }
