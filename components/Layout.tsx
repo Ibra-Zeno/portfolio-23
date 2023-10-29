19a9938b2eab1fb143299229521527e50727b63d
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-29 12:11:20 +0000

    bg image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Navbar from "./Nav";
 import Footer from "./Footer";
+import Image from "next/image";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 
 type LayoutProps = {
@@ -15,7 +16,18 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className=" flex-grow">{children}</main>
+      <main className="relative w-full">
+        <div className="fixed inset-0 -z-10 mx-auto h-screen w-screen">
+          <Image
+            src="/images/Bg/flat-mountains.svg"
+            className="h-full w-full object-cover object-top opacity-75"
+            alt="background landscape"
+            width={1400}
+            height={1000}
+          />
+        </div>
+        {children}
+      </main>
       <ThemeSwitcher />
       <Footer />
     </div>
