3a628aa90d63b172de893fa2edc955cb92ab6b55
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-29 18:35:34 +0000

    Design

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,28 +10,31 @@ const Hero: React.FC = () => {
       <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 md:px-12 lg:px-24 lg:py-10">
         <div className="mx-auto flex max-w-7xl flex-wrap items-center">
           <div className="mx-auto mb-8 flex flex-col items-start text-left md:mb-0 lg:mb-16 lg:w-1/2 lg:flex-grow lg:pr-6 xl:mt-0 xl:pr-24">
-            <h4 className="mb-4 rounded-lg py-1 text-sm dark:border-1 dark:border-main dark:bg-main/30 dark:px-2 ">
+            <h4 className="mb-6 rounded-lg border border-acc bg-sec/20 px-2 py-1 text-sm shadow-lg dark:border-main dark:bg-main/30 ">
               Welcome to my digital landscape 🍃
             </h4>
-            <h2 className="w-full text-center text-3xl font-bold tracking-tight  md:text-4xl lg:text-left lg:text-5xl xl:text-6xl">
-              Hey, I&apos;m{" "}
-              <span className="bg-gradient-to-r from-main to-acc bg-clip-text text-transparent dark:from-acc dark:to-sec">
-                Ibrahim
-              </span>
-            </h2>
-            <p className="mx-auto mt-6 max-w-prose text-center text-base leading-7 md:text-lg lg:text-left">
-              I&apos;m a full-stack developer of 3 years, focusing on front-end,
-              based in London. With a degree in Physics, I&apos;ve turned to
-              creating projects and of design theory. Beyond the code, I enjoy
-              cycling and Brazilian Jiu-Jitsu.
-            </p>
-            <div className="mx-auto mb-8 mt-8 flex w-full justify-center gap-x-6 lg:justify-start">
+            <div className="flex flex-col py-6 pt-3">
+              <h2 className="w-full text-center text-3xl font-bold tracking-tight  md:text-4xl lg:text-left lg:text-5xl xl:text-6xl">
+                Hey, I&apos;m{" "}
+                <span className="dark:from-pop1 dark:to-pop2 from-pop1 to-pop2 bg-gradient-to-r bg-clip-text text-transparent">
+                  {/* dark:from-[#00c6af] dark:to-[#258579] */}
+                  Ibrahim
+                </span>
+              </h2>
+              <p className="mx-auto mt-6 max-w-prose text-center text-base leading-7 md:text-lg lg:text-left">
+                I&apos;m a full-stack developer of 3 years, focusing on
+                front-end, based in London. With a degree in Physics, I&apos;ve
+                turned to creating projects and of design theory. Beyond the
+                code, I enjoy cycling and Brazilian Jiu-Jitsu.
+              </p>
+            </div>
+            <div className="mx-auto mb-8 mt-12 flex w-full justify-center gap-x-6 lg:justify-start">
               {contactIcons.map((icon, i) => (
                 <Link
                   key={i}
                   href={icon.href}
                   aria-label={icon.name}
-                  className="rounded-xl bg-sec p-2 transition-all duration-200 ease-in-out dark:bg-sec dark:text-acc"
+                  className="rounded-xl border border-[#adb13a]/80 bg-sec/90 p-2 text-background shadow-xl transition-all duration-200 ease-in-out dark:border-sec dark:bg-main/10 dark:text-foreground"
                 >
                   {icon.icons}
                 </Link>
