33e71fc40c615fb6a5eb51f645d2d7e00000f293
Ibrahim Kalam <95757707+Ibra-Zeno@users.noreply.github.com>
2023-10-29 18:34:11 +0000

    Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import Navbar from "@/components/Nav";
 import Hero from "@/components/Hero";
 import TechStack from "@/components/TechStack";
 import Projects from "@/components/Projects";
-import Footer from "@/components/Footer";
 import Layout from "@/components/Layout";
+import Head from "next/head";
 import About from "@/components/Contact";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ibrahim Kalam</title>
+        <meta name="description" content="Ibrahim Kalam's portfolio website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Layout>
         <Hero />
         <TechStack />
